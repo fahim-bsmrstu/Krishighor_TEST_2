93dccbf7ac5c722c3a07171536ac175dbfaa71a0
fahim-bsmrstu <fahimsikder01@gmail.com>
2016-02-15 14:57:59 +0600

    Added Session and SQLite

---
+++ app/src/main/java/com/example/klayton/krishighor_test_2/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.klayton.krishighor_test_2.app;
+
+/**
+ * Created by Klayton on 15-Feb-16.
+ */
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.159.1/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.159.1/android_login_api/register.php";
+
+}

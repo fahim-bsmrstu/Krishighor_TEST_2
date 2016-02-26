9cd6b4ff57afcb2069276ba9cc45f15cb54a6d8c
fahim-bsmrstu <fahimsikder01@gmail.com>
2016-02-26 15:14:50 +0600

    Registration

--- app/src/main/java/com/example/klayton/krishighor_test_2/reg/Login.java
+++ app/src/main/java/com/example/klayton/krishighor_test_2/reg/Login.java
@@ -25,14 +25,7 @@ public class Login extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         login = (Button)findViewById(R.id.button2);
         reg = (Button)findViewById(R.id.button3);
--- app/src/main/java/com/example/klayton/krishighor_test_2/reg/Register.java
+++ app/src/main/java/com/example/klayton/krishighor_test_2/reg/Register.java
@@ -7,21 +7,37 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.klayton.krishighor_test_2.MainActivity;
 import com.example.klayton.krishighor_test_2.R;
+import com.example.klayton.krishighor_test_2.app.AppConfig;
+import com.example.klayton.krishighor_test_2.app.AppController;
 import com.example.klayton.krishighor_test_2.helper.SQLiteHandler;
 import com.example.klayton.krishighor_test_2.helper.SessionManager;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
+
+    public static final String TAG = Register.class.getSimpleName();
 
     EditText e_name,e_user,e_pass,e_add,e_mob;
     Button reg;
-    ProgressDialog progressDialog;
+    private ProgressDialog progressDialog;
     SessionManager session;
     SQLiteHandler db;
 
@@ -37,7 +53,7 @@ public class Register extends AppCompatActivity {
         e_name =(EditText)findViewById(R.id.fullName);
         e_user =(EditText)findViewById(R.id.userName);
         e_pass =(EditText)findViewById(R.id.password);
-        e_add = (EditText)findViewById(R.id.editText);
+        e_add = (EditText)findViewById(R.id.location);
         e_mob =(EditText)findViewById(R.id.mobile);
         reg = (Button)findViewById(R.id.register);
 
@@ -82,7 +98,7 @@ public class Register extends AppCompatActivity {
 
                 if(!name.isEmpty() && !user_name.isEmpty() && !pass.isEmpty() && !address.isEmpty() && !mobile.isEmpty()){
 
-
+                    registerUser(name,user_name,pass,mobile,address);
 
 
                 }
@@ -104,5 +120,119 @@ public class Register extends AppCompatActivity {
 
 
     }
+
+
+    private void registerUser(final String name, final String user, final String pass, final String mob, final String location){
+
+
+
+        String tag_string_req = "req_register";
+
+        progressDialog.setMessage("Registering.....");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, AppConfig.URL_REGISTER, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean error = jsonObject.getBoolean("error");
+
+                    if(!error){
+
+                        JSONObject user = jsonObject.getJSONObject("user");
+                        String name = user.getString("name");
+                        String user_name = user.getString("user_name");
+                        String mob = user.getString("mobile");
+                        String location = user.getString("location");
+
+
+                       // db.addUser(name,user_name,mob,location);
+
+                        Toast.makeText(getApplicationContext(), "User Successfully Registered", Toast.LENGTH_LONG).show();
+
+
+                        // Launch Login Activity
+                        /*
+                        Intent i = new Intent(Register.this, Login.class);
+                        startActivity(i);
+
+                        */
+                        finish();
+
+                    } else {
+
+                        String errorMsg = jsonObject.getString("error_msg");
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams(){
+
+                Map<String, String>params = new HashMap<String, String>();
+
+                params.put("name",name);
+                params.put("user_name",user);
+                params.put("pass",pass);
+                params.put("mob",mob);
+                params.put("location",location);
+
+                return params;
+
+
+            }
+
+        };
+
+
+        // Adding requst to request queue
+        AppController.getInstance().addToRequestQueue(strReq,tag_string_req);
+
+
+
+
+
+
+
+
+    }
+
+    private void showDialog(){
+
+        if(!progressDialog.isShowing()){
+            progressDialog.show();
+        }
+    }
+
+    private void hideDialog(){
+
+        if(progressDialog.isShowing()){
+
+            progressDialog.dismiss();
+        }
+
+    }
+
+
+
 
 }
